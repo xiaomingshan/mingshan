f5710683a54abff7d6a3ad4fd66b4c6caf160f17
xiaomingshan <1290425230@qq.com>
2018-10-21 20:53:11 +0800

    11111111111
    22222222

--- app/src/main/java/com/cloud/easy/newgithub/MainActivity.java
+++ app/src/main/java/com/cloud/easy/newgithub/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.e("@@@","11111111111");
+        Log.e("@@@","22222");
     }
 }
